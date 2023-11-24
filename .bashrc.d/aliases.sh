7717522860820a20f1be9313528ae24c60146486
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2023-11-24 11:55:27 -0700

    bashrc.d/aliases.sh

---
+++ .bashrc.d/aliases.sh
@@ -0,0 +1,8 @@
+# Aliases
+
+# Long Format List
+alias ll="ls -AFhosv $*"
+alias cll="clear && ll"
+
+# Git
+alias githist="git log --pretty='%C(yellow)%h %C(cyan)%cd %Cblue%aN%C(auto)%d %Creset%s' --graph --date=short --date-order"
