8226d16125df57b86efa9bc242c14ae8ec67e2b2
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2023-12-02 22:22:16 -0700

    add codei alias

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -6,3 +6,6 @@ alias cll="clear && ll"
 
 # Git
 alias githist="git log --pretty='%C(yellow)%h %C(cyan)%cd %Cblue%aN%C(auto)%d %Creset%s' --graph --date=short --date-order"
+
+# Code Insiders
+alias code="code-insiders $*"
