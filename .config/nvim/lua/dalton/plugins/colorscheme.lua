ed117e11f3d3f9cd928a1a633316e9b426571333
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2023-12-22 13:54:51 -0700

    update colorscheme

--- .config/nvim/lua/dalton/plugins/colorscheme.lua
+++ .config/nvim/lua/dalton/plugins/colorscheme.lua
@@ -5,12 +5,24 @@ return {
 	-- 	vim.cmd([[colorscheme nightfly]])
 	-- end,
 
-	"rose-pine/neovim",
-	name = "rose-pine",
+	--	"rose-pine/neovim",
+	--	name = "rose-pine",
+	--	config = function()
+	--		require("rose-pine").setup({
+	--			--variant = "moon"
+	--			vim.cmd("colorscheme rose-pine"),
+	--			vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
+	--			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
+	--		})
+	--	end,
+
+	"shaunsignh/nord.nvim",
+	name = "nord",
 	config = function()
-		require("rose-pine").setup({
-			--variant = "moon"
+		require("nord").setup({
+			vim.cmd("colorscheme nord"),
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
 		})
-		vim.cmd("colorscheme rose-pine")
 	end,
 }
