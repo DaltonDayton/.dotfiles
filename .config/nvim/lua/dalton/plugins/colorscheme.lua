5bb3c21f6ce95f47297a09f37ec9a55cd75cb949
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2023-12-22 19:58:47 -0700

    fix colorscheme

--- .config/nvim/lua/dalton/plugins/colorscheme.lua
+++ .config/nvim/lua/dalton/plugins/colorscheme.lua
@@ -1,28 +1,15 @@
 return {
-	-- "bluz71/vim-nightfly-guicolors",
-	-- priority = 1000,
-	-- config = function()
-	-- 	vim.cmd([[colorscheme nightfly]])
-	-- end,
-
-	--	"rose-pine/neovim",
-	--	name = "rose-pine",
-	--	config = function()
-	--		require("rose-pine").setup({
-	--			--variant = "moon"
-	--			vim.cmd("colorscheme rose-pine"),
-	--			vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
-	--			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
-	--		})
-	--	end,
-
-	"shaunsignh/nord.nvim",
+	"shaunsingh/nord.nvim",
 	name = "nord",
 	config = function()
-		require("nord").setup({
-			vim.cmd("colorscheme nord"),
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
-		})
+		-- Load the Nord color scheme
+		vim.cmd("colorscheme nord")
+
+		-- Set background color for Normal and NormalFloat highlight groups
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+		-- Additional configuration for the Nord theme, if any
+		-- require("nord").setup({ ... })
 	end,
 }
