3d5a2c340d2752ad19a0b01c7b411dd7bb897e74
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-01-23 21:05:28 -0700

    Add vim-illuminate

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
     { "nvim-tree/nvim-web-devicons", lazy = true },
     { "MunifTanjim/nui.nvim", lazy = true },
-    { "nvim-lua/plenary.nvim", lazy = true }
+    { "nvim-lua/plenary.nvim", lazy = true },
 }
