869dc122b68dda7cbbc4209edbcb332a8d7adb9e
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-01-28 22:20:29 -0700

    add path to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,19 @@ end
 abbr --add cll clear_and_list
 
 abbr --add githist "git log --pretty='%C(yellow)%h %C(cyan)%cd %Cblue%aN%C(auto)%d %Creset%s' --graph --date=short --date-order"
+
+
+# Function to conditionally add directories to PATH
+function add_to_path
+    for dir in $argv
+        if test -d $dir
+            if not contains $dir $PATH
+                set -g PATH $dir $PATH
+            end
+        end
+    end
+end
+
+# Add directories to PATH
+add_to_path $HOME/.local/bin $HOME/bin
+
