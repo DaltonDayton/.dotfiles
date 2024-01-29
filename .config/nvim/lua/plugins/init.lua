4379930c1b8c778282e3b93bb3e042b9a642266b
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-01-28 22:22:31 -0700

    add vim-rails and copilot

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,4 +2,6 @@ return {
     { "nvim-tree/nvim-web-devicons", lazy = true },
     { "MunifTanjim/nui.nvim", lazy = true },
     { "nvim-lua/plenary.nvim", lazy = true },
+    { "tpope/vim-rails", lazy = true },
+    { "github/copilot.vim", lazy = true },
 }
