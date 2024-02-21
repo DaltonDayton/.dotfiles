4d3e571db3ce0775920a70a941706b0fa406cbf0
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-02-21 14:11:42 -0700

    restart nvim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,16 @@
 require "core"
 
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
-
-dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
+
 require "plugins"
---
+++ .config/nvim/lua/core/config.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.options = {}
+
+M.plugins = ""
+
+M.lazy_nvim = require "plugins.configs.lazy_nvim"
+
+M.keymaps = require "core.keymaps"
+
+return M
