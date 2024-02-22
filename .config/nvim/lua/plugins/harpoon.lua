7dc0802084ec93c6d2bbd569ff6bbc555b06eb48
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-02-22 04:48:27 -0700

    remove everything except colorscheme/harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,7 @@
 return {
     'ThePrimeagen/harpoon',
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local mark = require('harpoon.mark')
         local ui = require('harpoon.ui')
