e35c70f679596efae2eb74ed0880a0cb030f2943
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-02-22 06:02:54 -0700

    add telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "BurntSushi/ripgrep",
+        "nvim-telescope/telescope-fzf-native.nvim",
+        "sharkdp/fd"
+    },
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Telescope: Find Files" },
+        { "<leader>fd", "<cmd>Telescope find_files find_command=rg,--no-ignore,--hidden,--files<CR>", desc = "Telescope: Find Files including hidden/.gitignore" },
+        { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Telescope: Live Grep" },
+        { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Telescope: Buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Telescope: Help Tags" },
+    }
+}
