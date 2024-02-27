3e4f4fbf6c86584da6219cfae1a8ca8c6ed19971
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-02-26 17:51:15 -0700

    add desc

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
         local mark = require('harpoon.mark')
         local ui = require('harpoon.ui')
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
+        vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Add to list" })
         vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
         vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
