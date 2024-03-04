94a376cd14e72558cf3dd3a99ce2d7428c7f61d8
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-03-03 21:18:17 -0700

    add obsidian

--- .config/nvim/lua/plugins/lsp_zero.lua
+++ .config/nvim/lua/plugins/lsp_zero.lua
@@ -71,14 +71,14 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm({ select = false }),
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
             }),
             completion = {
                 completeopt = 'menu,menuone,noselect',
-                preselect = cmp.PreselectMode.Item,
+                -- preselect = cmp.PreselectMode.Item,
             },
         })
     end
