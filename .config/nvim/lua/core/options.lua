4faae2d6dddf00467817877be035cfe3e5099063
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-03-03 17:08:47 -0700

    remove extra and update arch setup

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,36 +1,42 @@
--- Line Numbers
-vim.opt.nu = true
--- Relative Line Numbers
-vim.opt.relativenumber = true
+-- Line number settings
+vim.opt.nu = true               -- Enable line numbers
+vim.opt.relativenumber = true   -- Enable relative line numbers
 
--- Indenting
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 0
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+-- Indentation settings
+vim.opt.tabstop = 8             -- Set the number of space characters inserted for a tab
+vim.opt.softtabstop = 0         -- Set the number of spaces a <Tab> counts for while performing editing operations, like inserting a tab or using <BS>
+vim.opt.shiftwidth = 4          -- Set the number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true        -- Convert tabs to spaces
+vim.opt.smartindent = true      -- Enable smart indentation
 
+-- Disable text wrapping
 vim.opt.wrap = false
 
--- no backups from vim, give undotree access to long running undos
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+-- Backup and undo settings
+vim.opt.swapfile = false        -- Disable swap file creation
+vim.opt.backup = false          -- Disable backup file creation
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"  -- Set the directory for undo files
+vim.opt.undofile = true         -- Enable persistent undo
 
--- No sticky highligting, but enable incremental search highlighting
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- Search settings
+vim.opt.hlsearch = false        -- Disable search result highlighting
+vim.opt.incsearch = true        -- Enable incremental search
 
--- Good colors
-vim.opt.termguicolors = true
+-- Color settings
+vim.opt.termguicolors = true    -- Enable true color support
 
--- Keeps so many lines on the screen when scrolling
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+-- Scrolling settings
+vim.opt.scrolloff = 8           -- Keep 8 lines above/below the cursor when scrolling
+vim.opt.signcolumn = "yes"      -- Always show the sign column, avoiding text shifting
 
-vim.opt.updatetime = 50
-vim.opt.timeoutlen = 100
+-- File name settings
+vim.opt.isfname:append("@-@")   -- Adjust 'isfname' option to consider '@' as part of a file name
 
-vim.opt.colorcolumn = "80"
+-- Performance settings
+vim.opt.updatetime = 50         -- Faster completion (4000ms default)
+vim.opt.timeoutlen = 100        -- Time in milliseconds to wait for a mapped sequence to complete
+
+-- Editor display settings
+vim.opt.colorcolumn = "80"      -- Highlight column 80 to encourage limiting line length
+vim.opt.conceallevel = 1        -- Hide certain elements (like markdown links) in a minimal manner
+
--- setupArchEnvironment.sh
+++ setupArchEnvironment.sh
@@ -38,20 +38,45 @@ if [ "$OS" == "Linux" ]; then
     echo ""
     # Declare an array of packages to be checked and installed
     packages=(
-        "neovim"
-        "tmux"
-        "solaar"
-        "curl"
-        "ripgrep"
-        "zsh"
-        "zoxide"
-        "fzf"
-#        "swaybg"
-#        "grim"
-#        "slurp"
-#        "vlc"
-#        "ffmpeg"
+        # Terminal utilities
+        "tmux" # Terminal multiplexer for multiple terminal sessions
+        "zsh" # Enhanced UNIX shell with improved features and support for plugins
+        "zoxide" # Fast directory navigation tool
+        "fzf" # Command-line fuzzy finder for finding files and other items
+
+        # Development tools
+        "base-devel" # Group containing tools essential for building (compiling and linking)
+        "git" # Distributed version control system
+        "curl" # Tool for transferring data from or to a server with various protocols
+        "gnupg" # Encryption software that provides secure communication and data storage
+
+        # Build systems
+        "ninja" # Small build system with a focus on speed, designed for speed
+        "cmake" # Cross-platform family of tools designed to build, test, and package software
+        "autoconf" # Generates scripts to automatically configure software source code packages
+        "automake" # Generates Makefile.in files compliant with the GNU Coding Standards
+        "bison" # General-purpose parser generator that converts an annotated context-free grammar into a C program
+
+        # Libraries for development
+        "openssl" # Toolkit for the Transport Layer Security (TLS) and Secure Sockets Layer (SSL) protocols
+        "libyaml" # Library for parsing and emitting data in YAML format
+        "readline" # Library providing a set of functions for use by applications that allow users to edit command lines
+        "zlib" # Compression library used for data compression
+        "gmp" # Free library for arbitrary precision arithmetic
+        "ncurses" # Libraries for building text-based user interfaces
+        "libffi" # Provides a portable, high level programming interface to various calling conventions
+        "gdbm" # Library for GNU database manager, provides database functions
+        "db" # Oracle Berkeley DB, provides embedded database support
+        "util-linux" # Miscellaneous system utilities for Linux
+
+        # Programming languages and compilers
+        "rust" # Systems programming language focused on safety and concurrency
+
+        # Fonts
+        "ttf-font-awesome"
+        "noto-fonts-emoji"
     )
+
 
     # Loop through the array and check/install packages
     for package in "${packages[@]}"; do
@@ -73,12 +98,15 @@ if [ "$OS" == "Linux" ]; then
     # Declare an array of symlinks to be created
     symlinks=(
         "$HOME/.gitconfig:$HOME/.dotfiles/.gitconfig"
+        "$HOME/.bashrc:$HOME/.dotfiles/.bashrc"
         "$HOME/.zshrc:$HOME/.dotfiles/.zshrc"
+        "$HOME/.vimrc:$HOME/.dotfiles/.vimrc"
         "$HOME/.config/nvim:$HOME/.dotfiles/.config/nvim"
         "$HOME/.config/tmux:$HOME/.dotfiles/.config/tmux"
-        "$HOME/.config/solaar:$HOME/.dotfiles/.config/solaar"
-#        "$HOME/.config/hypr:$HOME/.dotfiles/.config/hypr"
-#        "$HOME/.config/kitty:$HOME/.dotfiles/.config/kitty"
+        "$HOME/.config/kitty/kitty.conf:$HOME/.dotfiles/.config/kitty/kitty.conf"
+        "$HOME/.config/hypr/hyprland.conf:$HOME/.dotfiles/.config/hypr/hyprland.conf"
+        "$HOME/.config/hypr/keybindings.conf:$HOME/.dotfiles/.config/hypr/keybindings.conf"
+        "$HOME/.config/hypr/monitors.conf:$HOME/.dotfiles/.config/hypr/monitors.conf"
     )
 
     # Loop through the array and create symlinks
@@ -86,16 +114,66 @@ if [ "$OS" == "Linux" ]; then
         source="${symlink_pair#*:}"
         destination="${symlink_pair%%:*}"
 
-        # Check if the symlink exists and is not broken
-        if [ ! -e "$destination" ]; then
+        # Check if the destination exists and is not a symlink
+        if [ -e "$destination" ] && [ ! -L "$destination" ]; then
+            echo "Non-symlink file found at $destination. Removing and creating symlink."
+            rm "$destination"
+            ln -s "$source" "$destination"
+        elif [ -L "$destination" ]; then
+            echo ">>> Symlink already exists at $destination. Skipping."
+        else
             echo "Creating symlink for $destination"
             ln -s "$source" "$destination"
-        else
-            echo ">>> $destination symlink already exists"
         fi
     done
 
-    # TODO: https://asdf-vm.com/guide/getting-started.html
+    echo ""
+    echo "============================="
+    echo "Installing Neovim from source"
+    echo "============================="
+    echo ""
+    # Check if Neovim is already installed
+    if command -v nvim &>/dev/null; then
+        echo "Neovim is already installed"
+    else
+        echo "Building Neovim from source"
+        # Clone Neovim's repository if it doesn't exist
+        if [ ! -d "$HOME/neovim" ]; then
+            git clone https://github.com/neovim/neovim $HOME/neovim
+        fi
+        cd $HOME/neovim
+        # Build and install Neovim
+        make CMAKE_BUILD_TYPE=Release
+        sudo make install
+        cd $HOME/.dotfiles
+    fi
+
+    echo ""
+    echo "================================================"
+    echo "===== Set up asdf for node/ruby management ====="
+    echo "================================================"
+    echo ""
+    # Check if asdf is already installed
+    if command -v asdf &>/dev/null; then
+        echo "asdf is already installed"
+    else
+        echo "Building asdf from source"
+        # Clone asdf's repository if it doesn't exist
+        if [ ! -d "$HOME/.asdf" ]; then
+            git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+        fi
+    fi
+    . "$HOME/.asdf/asdf.sh"
+    . "$HOME/.asdf/completions/asdf.bash"
+
+    asdf plugin add nodejs
+    asdf install nodejs latest
+    asdf global nodejs latest
+
+    asdf plugin add ruby
+    asdf install ruby latest
+    asdf global ruby latest
+
 
 #    echo ""
 #    echo "###############"
