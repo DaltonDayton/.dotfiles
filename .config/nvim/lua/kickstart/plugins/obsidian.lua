a405c1e0ffb0212bf1c123d1e5496259e2db399e
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-03-28 14:07:38 -0600

    obsidian/lint

--- .config/nvim/lua/kickstart/plugins/obsidian.lua
+++ .config/nvim/lua/kickstart/plugins/obsidian.lua
@@ -165,19 +165,19 @@ return {
     },
     mappings = {
       -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-      -- ["gf"] = {
-      --     action = function()
-      --         return require("obsidian").util.gf_passthrough()
-      --     end,
-      --     opts = { noremap = false, expr = true, buffer = true },
-      -- },
-      -- -- Toggle check-boxes.
-      -- ["<leader>ch"] = {
-      --     action = function()
-      --         return require("obsidian").util.toggle_checkbox()
-      --     end,
-      --     opts = { buffer = true },
-      -- },
+      ['gf'] = {
+        action = function()
+          return require('obsidian').util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ['<leader>ch'] = {
+        action = function()
+          return require('obsidian').util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
     },
     notes_subdir = 'Zettelkasten',
     new_notes_location = 'notes_subdir',
