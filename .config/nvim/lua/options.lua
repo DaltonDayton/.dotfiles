b124e131c4f8ee1002aa74de14a1ac2e974b0f5b
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-04-10 12:55:47 -0600

    start moving to individual files. move options, keymaps, and autocmds, also lsp and telescope

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,87 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 50
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Convert tabs to spaces
+vim.opt.expandtab = true
+
+-- Enable true color support
+vim.opt.termguicolors = true
+
+-- Highlight column 80 to encourage limiting line length
+-- vim.opt.colorcolumn = '80'
+
+-- Hide certain elements (like markdown links) in a minimal manner
+vim.opt.conceallevel = 2
+
+-- Backup and undo settings
+vim.opt.swapfile = false -- Disable swap file creation
+vim.opt.backup = false -- Disable backup file creation
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir' -- Set the directory for undo files
+vim.opt.undofile = true -- Enable persistent undo
