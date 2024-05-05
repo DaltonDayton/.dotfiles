f53d2ac20bd6d1f0973a14ce8a6cd3fa65cc482a
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-05-04 23:16:36 -0600

    add autoformatting toggle and keymap

--- .config/nvim/lua/dalton/core/autocmds.lua
+++ .config/nvim/lua/dalton/core/autocmds.lua
@@ -2,28 +2,44 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
+-- Command to disable formatting
 vim.api.nvim_create_user_command("FormatDisable", function(args)
-  if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
-    vim.b.disable_autoformat = true
-  else
-    vim.g.disable_autoformat = true
-  end
+    if args.bang then
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
 end, {
-  desc = "Disable autoformat-on-save",
-  bang = true,
+    desc = "Disable autoformat-on-save",
+    bang = true,
 })
 
+-- Command to enable formatting
 vim.api.nvim_create_user_command("FormatEnable", function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
 end, {
-  desc = "Re-enable autoformat-on-save",
+    desc = "Re-enable autoformat-on-save",
+})
+
+-- Command to toggle formatting
+vim.api.nvim_create_user_command("FormatToggle", function()
+    if vim.g.disable_autoformat or vim.b.disable_autoformat then
+        -- If formatting is currently disabled, enable it
+        vim.cmd("FormatEnable")
+        print("Autoformatting is now enabled.")
+    else
+        -- If formatting is currently enabled, disable it
+        vim.cmd("FormatDisable")
+        print("Autoformatting is now disabled.")
+    end
+end, {
+    desc = "Toggle autoformat-on-save using existing enable/disable commands",
 })
