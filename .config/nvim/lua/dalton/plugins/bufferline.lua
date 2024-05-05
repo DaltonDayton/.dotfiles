03b634590e997c03da2f860a13d4871469947702
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-05-04 23:17:57 -0600

    temp disable bufferline until next release

--- .config/nvim/lua/dalton/plugins/bufferline.lua
+++ .config/nvim/lua/dalton/plugins/bufferline.lua
@@ -2,6 +2,7 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
+  keys = { "<leader>zz" }, -- TODO: Remove this when v4.6 is available.
   opts = {
     options = {
       mode = "tabs",
