ef7307747569f5fee508b71d17991e2602fc7fd0
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-05-30 19:16:34 -0600

    update setupWSLEnvironment.sh

--- scripts/setupWSLEnvironment.sh
+++ scripts/setupWSLEnvironment.sh
@@ -1,43 +1,69 @@
 #!/bin/bash
 
-# Function to check if a package is installed
-is_package_installed() {
-    dpkg -s "$1" &> /dev/null
-}
-
-# Function to install a package if it's not already installed
-install_package() {
-    if is_package_installed "$1"; then
-        echo "$1 is already installed."
-    else
-        echo "Installing $1..."
-        sudo apt-get install -y "$1"
-    fi
-}
-
-# Function to create a symlink if it doesn't already exist
-create_symlink() {
-    if [ -L "$2" ]; then
-        echo "Symlink $2 already exists."
-    else
-        echo "Creating symlink from $1 to $2..."
-        ln -s "$1" "$2"
-    fi
-}
-
-# Update and upgrade packages
+echo ""
+echo "============================"
+echo "=== Updating Packages... ==="
+echo "============================"
+echo ""
 echo "Updating and upgrading packages..."
 sudo apt-get update && sudo apt-get upgrade -y
 
-# Install packages
-install_package git
-install_package curl
-install_package vim
+echo ""
+echo "=============================="
+echo "=== Installing Packages... ==="
+echo "=============================="
+echo ""
+# Define packages on separate lines for easy modification
+packages=(
+	vim
+)
+
+# Loop through the packages and install if not already installed
+for pkg in "${packages[@]}"; do
+    if ! dpkg -s $pkg &> /dev/null; then
+        echo "Installing $pkg..."
+        sudo apt-get install -y $pkg
+    else
+        echo "$pkg is already installed."
+    fi
+done
+
+echo ""
+echo "=============================="
+echo "=== Setting up Symlinks... ==="
+echo "=============================="
+echo ""
+
+# Define symlink mappings from source to destination
+declare -A links=(
+    ["$HOME/.dotfiles/_home/.gitconfig"]="$HOME/.gitconfig"
+    # ["$HOME/.dotfiles/_config/nvim"]="$HOME/.config/nvim"
+)
 
 # Create symlinks
-create_symlink "$HOME/.dotfiles/_home/.gitconfig" "$HOME/.gitconfig"
+for src in "${!links[@]}"; do
+    dest="${links[$src]}"
 
+    # Check if the destination directory exists; if not, create it
+    dest_dir=$(dirname "$dest")
+    if [ ! -d "$dest_dir" ]; then
+        mkdir -p "$dest_dir"
+        echo "Created directory $dest_dir"
+    fi
+
+    # Create the symlink if it does not exist or update if incorrect
+    if [ ! -L "$dest" ] || [ "$(readlink "$dest")" != "$src" ]; then
+        ln -sfn "$src" "$dest"
+        echo "Created or updated symlink from $src to $dest"
+    else
+        echo "Symlink from $src to $dest already correctly set."
+    fi
+done
 # Add more package installations and symlink creations as needed
 
-echo "Setup complete."
+echo ""
+echo "======================="
+echo "=== Setup Complete. ==="
+echo "======================="
+echo ""
 
