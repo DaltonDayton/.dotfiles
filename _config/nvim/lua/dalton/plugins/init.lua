c14ef0d7cda33106443d1629a003444b1b465ed0
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-07-03 14:03:22 -0600

    add neotest and lazydev

--- _config/nvim/lua/dalton/plugins/init.lua
+++ _config/nvim/lua/dalton/plugins/init.lua
@@ -4,18 +4,17 @@ return {
   {
     "RRethy/vim-illuminate",
     config = function()
-      vim.keymap.set('n', '<leader>in', function() require('illuminate').goto_next_reference(true) end,
-        { desc = "Go to next reference" })
-      vim.keymap.set('n', '<leader>ip', function() require('illuminate').goto_prev_reference(true) end,
-        { desc = "Go to previous reference" })
-      vim.keymap.set('n', '<leader>is', function() require('illuminate').textobj_select() end,
-        { desc = "Select text object" })
-    end
+      -- stylua: ignore start
+      vim.keymap.set('n', '<leader>in', function() require('illuminate').goto_next_reference(true) end, { desc = "Go to next reference" })
+      vim.keymap.set('n', '<leader>ip', function() require('illuminate').goto_prev_reference(true) end, { desc = "Go to previous reference" })
+      vim.keymap.set('n', '<leader>is', function() require('illuminate').textobj_select() end, { desc = "Select text object" })
+      -- stylua: ignore end
+    end,
   },
   {
     "szw/vim-maximizer",
     keys = {
       { "<leader>ms", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
     },
-  }
+  },
 }
