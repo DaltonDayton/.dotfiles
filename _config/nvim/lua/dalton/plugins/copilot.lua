a5128f8c18fb417dd5841f250af851fed3f2fb6c
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-07-21 11:19:17 -0600

    change from notify to update

--- _config/nvim/lua/dalton/plugins/copilot.lua
+++ _config/nvim/lua/dalton/plugins/copilot.lua
@@ -15,20 +15,21 @@ return {
     branch = "canary",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
     opts = {
-      show_help = "yes",         -- Show help text for CopilotChatInPlace, default: yes
-      debug = false,             -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
       disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
-      language = "English",      -- Copilot answer language settings when using default prompts. Default language is English.
+      language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
       -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
       -- temperature = 0.1,
     },
     build = function()
-      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+      vim.cmd("UpdateRemotePlugins")
     end,
     event = "VeryLazy",
+    -- stylua: ignore start
     keys = {
       { "<leader>ccc", ":CopilotChat ",                     mode = { "n", "v" }, desc = "CopilotChat - Open chat window with optional input" },
       { "<leader>cce", "<cmd>CopilotChatExplain<cr>",       mode = { "n", "v" }, desc = "CopilotChat - Explain code" },
@@ -37,6 +38,7 @@ return {
       { "<leader>ccf", "<cmd>CopilotChatFixDiagnostic<cr>", mode = { "n", "v" }, desc = "CopilotChat - Fix diagnostic" },
       { "<leader>ccr", "<cmd>CopilotChatReset<cr>",         mode = { "n", "v" }, desc = "CopilotChat - Reset chat history and clear buffer" },
     },
+    -- stylua: ignore end
   },
   { -- Copilot Cmp
     "zbirenbaum/copilot-cmp",
