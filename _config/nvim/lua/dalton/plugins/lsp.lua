623e5354ba5d5abd77388766077fd5a38391716b
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-07-21 11:16:33 -0600

    add ruby-lsp to ensure_installed

--- _config/nvim/lua/dalton/plugins/lsp.lua
+++ _config/nvim/lua/dalton/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
     lazy = true,
     config = false,
     init = function()
@@ -11,21 +11,21 @@ return {
     end,
   },
   {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     lazy = false,
     config = true,
   },
 
   -- Autocompletion
   {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
       {
-        "hrsh7th/cmp-buffer",           -- source for text in buffer
-        "hrsh7th/cmp-path",             -- source for file system paths
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path", -- source for file system paths
         "rafamadriz/friendly-snippets", -- useful snippets
-        'onsails/lspkind-nvim',         -- vs-code like pictograms
+        "onsails/lspkind-nvim", -- vs-code like pictograms
         {
           "L3MON4D3/LuaSnip",
           -- follow latest release.
@@ -37,23 +37,23 @@ return {
     },
     config = function()
       -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
+      local lsp_zero = require("lsp-zero")
       lsp_zero.extend_cmp()
 
       -- And you can configure cmp even more, if you want to.
-      local cmp = require('cmp')
+      local cmp = require("cmp")
       local cmp_action = lsp_zero.cmp_action()
 
       -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-      require('luasnip.loaders.from_vscode').lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         sources = {
-          { name = 'copilot' },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-          { name = 'path' },
+          { name = "copilot" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
         },
         cmp.setup.filetype({ "sql", "mysql" }, {
           sources = {
@@ -62,15 +62,15 @@ return {
           },
         }),
         mapping = cmp.mapping.preset.insert({
-          ['<C-a>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ["<C-a>"] = cmp.mapping.complete(),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
         }),
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         window = {
@@ -79,31 +79,31 @@ return {
         },
         -- formatting = lsp_zero.cmp_format({ details = true }),
         formatting = {
-          fields = { 'abbr', 'kind', 'menu' },
-          format = require('lspkind').cmp_format({
-            mode = 'symbol_text',  -- show only symbol annotations
-            maxwidth = 50,         -- prevent the popup from showing more than provided characters
+          fields = { "abbr", "kind", "menu" },
+          format = require("lspkind").cmp_format({
+            mode = "symbol_text", -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters
             symbol_map = { Copilot = "" },
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-          })
-        }
+            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+          }),
+        },
       })
-    end
+    end,
   },
 
   -- LSP
   {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
+    "neovim/nvim-lspconfig",
+    cmd = { "LspInfo", "LspInstall", "LspStart" },
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'williamboman/mason-lspconfig.nvim' },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "williamboman/mason-lspconfig.nvim" },
       { "jay-babu/mason-nvim-dap.nvim" },
     },
     config = function()
       -- This is where all the LSP shenanigans will live
-      local lsp_zero = require('lsp-zero')
+      local lsp_zero = require("lsp-zero")
       lsp_zero.extend_lspconfig()
 
       --- if you want to know more about lsp-zero and mason.nvim
@@ -117,9 +117,10 @@ return {
         -- Helper function to keep keymap line short
         local function set_keymap(key, func, desc)
           local opts = { buffer = bufnr, noremap = true, silent = true, desc = desc }
-          vim.keymap.set('n', key, func, opts)
+          vim.keymap.set("n", key, func, opts)
         end
 
+        -- stylua: ignore start
         set_keymap('K', vim.lsp.buf.hover, '[LSP] Hover Info')
         set_keymap('gd', require('telescope.builtin').lsp_definitions, '[LSP/T] Go to Definition')
         set_keymap('gD', vim.lsp.buf.declaration, '[LSP] Go to Declaration')
@@ -133,25 +134,27 @@ return {
         set_keymap('gl', vim.diagnostic.open_float, '[LSP] Show Diagnostics')
         set_keymap('[d', function() vim.diagnostic.jump({ count = -1 }) end, '[LSP] Previous Diagnostic')
         set_keymap(']d', function() vim.diagnostic.jump({ count = 1 }) end, '[LSP] Next Diagnostic')
+        -- stylua: ignore end
       end)
 
       lsp_zero.set_sign_icons({
-        error = '', -- Heavy Ballot X Mark
-        warn = '', -- Warning Sign
-        hint = '', -- Light Bulb
-        info = '' -- Information
+        error = "", -- Heavy Ballot X Mark
+        warn = "", -- Warning Sign
+        hint = "", -- Light Bulb
+        info = "", -- Information
       })
 
-      require('mason-lspconfig').setup({
+      require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
           "pyright",
+          "ruby_lsp",
         },
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
           function(server_name)
-            require('lspconfig')[server_name].setup({})
+            require("lspconfig")[server_name].setup({})
           end,
 
           -- Custom handlers example
@@ -165,10 +168,10 @@ return {
           -- end,
 
           -- See https://lsp-zero.netlify.app/v3.x/language-server-configuration.html
-        }
+        },
       })
 
-      require('mason-nvim-dap').setup({
+      require("mason-nvim-dap").setup({
         ensure_installed = {
           "python",
         },
@@ -177,7 +180,7 @@ return {
             -- all sources with no handler get passed here
 
             -- Keep original functionality
-            require('mason-nvim-dap').default_setup(config)
+            require("mason-nvim-dap").default_setup(config)
           end,
           -- python = function(config)
           --   config.adapters = {
@@ -191,8 +194,7 @@ return {
           --   require('mason-nvim-dap').default_setup(config) -- don't forget this!
           -- end,
         },
-
       })
-    end
-  }
+    end,
+  },
 }
