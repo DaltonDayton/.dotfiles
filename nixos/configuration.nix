a313f6234b80e34407481543a51dc239d35c8f11
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-09-20 20:07:26 -0600

    switch to zen

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,9 @@
   };
 
   config = {
+    # kernel
+    boot.kernelPackages = pkgs.linuxPackages_zen;
+
     # Bootloader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
