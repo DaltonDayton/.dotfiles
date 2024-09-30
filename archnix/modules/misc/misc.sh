c36c4b2a3f29cde8e3707311a2ca206f1bd3c9b2
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-09-29 22:35:45 -0600

    add gaming and misc. update method that updates versions. update git to use bat

---
+++ archnix/modules/misc/misc.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Function to install the module
+function install_misc() {
+  # Define the list of packages required for this module
+  local packages=(
+    "bat"
+  )
+
+  # Install the packages using the install_packages function
+  install_packages "${packages[@]}"
+
+  # No extra config for misc. If needed, break into its own module.
+}
