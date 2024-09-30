76e3ecf929c1c437336795501f5fdb6c54599bda
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-09-30 17:10:24 -0600

    update hyprland to only use userprefs and hyprdots for the rest

--- archnix/modules/hyprland/hyprland.sh
+++ archnix/modules/hyprland/hyprland.sh
@@ -4,10 +4,8 @@
 function install_hyprland() {
   # Define the list of packages required for this module
   local packages=(
-    "wl-clipboard"
-    "nvidia-dkms"
-    "nvidia-utils"
-    "egl-wayland"
+    "swayidle"
+    "sway-audio-idle-inhibit-git"
   )
 
   # Install the packages using the install_packages function
@@ -20,11 +18,15 @@ function install_hyprland() {
 # Function to configure the module
 function configure_hyprland() {
   # These can be duplicated if multiple iterations need to be symlinked
-  CONFIG_SOURCE="$MODULES_DIR/hyprland/hypr"
-  CONFIG_DEST="$HOME/.config/hypr"
+  CONFIG_SOURCE="$MODULES_DIR/hyprland/hypr/userprefs.conf"
+  CONFIG_DEST="$HOME/.config/hypr/userprefs.conf"
 
   symlink_config "$CONFIG_SOURCE" "$CONFIG_DEST"
 
   # Additional configuration steps can be added here
   # For example, setting environment variables, running setup scripts, etc.
+
+  # TODO: install HyDE via install script, then run hyde cli?
+  # Or maybe just import themes using cli:
+  # https://github.com/HyDE-Project/hyde-gallery
 }
