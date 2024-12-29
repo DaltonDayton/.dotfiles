af1f7d71dff0e96fcdf07418dc6f590a7463b147
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-12-29 13:08:17 -0700

    move git user to env

--- arch/modules/git/git.sh
+++ arch/modules/git/git.sh
@@ -20,8 +20,24 @@ function configure_git() {
   # These can be duplicated if multiple iterations need to be symlinked
   CONFIG_SOURCE="$MODULES_DIR/git/config/.gitconfig"
   CONFIG_DEST="$HOME/.gitconfig"
-
   symlink_config "$CONFIG_SOURCE" "$CONFIG_DEST"
+
+  local user_config="$HOME/.gitconfig_default_user"
+
+  if [[ -z "$GIT_NAME" ]]; then
+    echo "Error: GIT_NAME is not set in .env. Please add your name to the .env file."
+    exit 1
+  fi
+  if [[ -z "$GIT_EMAIL" ]]; then
+    echo "Error: GIT_EMAIL is not set in .env. Please add your email to the .env file."
+    exit 1
+  fi
+
+  echo "[user]" > "$user_config"
+  echo "  name = $GIT_NAME" >> "$user_config"
+  echo "  email = $GIT_EMAIL" >> "$user_config"
+
+  echo "Git configured for $CONTEXT context."
 
   # Additional configuration steps can be added here
   # For example, setting environment variables, running setup scripts, etc.
