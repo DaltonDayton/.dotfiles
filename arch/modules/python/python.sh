05f909ae4e4b4e58b6e0e16b1c1b678b0a7e85fe
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-07-18 15:17:44 -0600

    comment out most of python

--- arch/modules/python/python.sh
+++ arch/modules/python/python.sh
@@ -4,33 +4,33 @@
 function install_python() {
   # Define the list of packages required for this module
   local packages=(
-    "python"
-    "python-poetry"
+    # "python"
+    # "python-poetry"
   )
 
   # Install the packages using the install_packages function
   install_packages "${packages[@]}"
 
   # Proceed to configuration
-  configure_python
+  # configure_python
 }
 
 # Function to configure the module
-function configure_python() {
-  # These can be duplicated if multiple iterations need to be symlinked
-  # CONFIG_SOURCE="$MODULES_DIR/python/config/files"
-  # CONFIG_DEST="$HOME/.config/python"
-  #
-  # symlink_config "$CONFIG_SOURCE" "$CONFIG_DEST"
+# function configure_python() {
+# These can be duplicated if multiple iterations need to be symlinked
+# CONFIG_SOURCE="$MODULES_DIR/python/config/files"
+# CONFIG_DEST="$HOME/.config/python"
+#
+# symlink_config "$CONFIG_SOURCE" "$CONFIG_DEST"
 
-  # Additional configuration steps can be added here
-  # For example, setting environment variables, running setup scripts, etc.
+# Additional configuration steps can be added here
+# For example, setting environment variables, running setup scripts, etc.
 
-  # Check if the virtualenvs.in-project is already set to true
-  if ! poetry config --list | grep -q 'virtualenvs.in-project = true'; then
-    poetry config virtualenvs.in-project true
-    echo "Virtualenvs are now set to be created in the project directory."
-  else
-    echo "Virtualenvs are already set to be created in the project directory."
-  fi
-}
+# Check if the virtualenvs.in-project is already set to true
+# if ! poetry config --list | grep -q 'virtualenvs.in-project = true'; then
+#   poetry config virtualenvs.in-project true
+#   echo "Virtualenvs are now set to be created in the project directory."
+# else
+#   echo "Virtualenvs are already set to be created in the project directory."
+# fi
+# }
