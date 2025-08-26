8bbea1c1adc819e86e3f7cd2680ebc86e1eb97db
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-08-25 19:20:46 -0600

    add javascript/node debugging

--- shared_configs/nvim/lua/dalton/plugins/dap.lua
+++ shared_configs/nvim/lua/dalton/plugins/dap.lua
@@ -52,6 +52,21 @@ return {
         ui.close()
       end
 
+      -- Add Node.js adapter configuration
+      -- Run with `node --inspect your-app.js`
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+
       dap.configurations.javascript = {
         {
           name = "Launch",
@@ -60,6 +75,13 @@ return {
           program = "${file}",
           cwd = "${workspaceFolder}",
           sourceMaps = true,
+        },
+        {
+          name = "Attach",
+          type = "pwa-node",
+          request = "attach",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
         },
       }
 
