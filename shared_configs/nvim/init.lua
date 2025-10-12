791b2b25e13fa1ba196a61a4aa9213e975d8a054
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-10-11 20:09:55 -0600

    swap to lazyvim

--- shared_configs/nvim/init.lua
+++ shared_configs/nvim/init.lua
@@ -1,2 +1,2 @@
-require("dalton.core")
-require("dalton.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- shared_configs/nvim/lua/dalton/plugins/zenmode.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "folke/zen-mode.nvim",
-    opts = {
-        -- https://github.com/folke/zen-mode.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
-    }
-}
