de7fa53f49bd9c73fa3d62c4718fc671ce8f1ce5
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-10-25 16:16:25 -0600

    core and lsp

---
+++ shared_configs/nvimtest/lua/plugins/lsp/mason.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "ts_ls",
+        "html",
+        "cssls",
+        "pyright",
+        "eslint",
+      },
+    },
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = {
+          {
+            "mason-org/mason-lspconfig.nvim",
+            opts = {},
+            dependencies = {
+              { "mason-org/mason.nvim", opts = {} },
+              "neovim/nvim-lspconfig",
+            },
+          },
+        },
+      },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+      },
+    },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+  },
+}
