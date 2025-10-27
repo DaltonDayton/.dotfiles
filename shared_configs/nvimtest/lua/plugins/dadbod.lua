2ab59bcf6e78a9c8dcaa067f0d8a7395dee940b0
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-10-26 19:05:08 -0600

    dadbod

---
+++ shared_configs/nvimtest/lua/plugins/dadbod.lua
@@ -0,0 +1,10 @@
+return {
+  "tpope/vim-dadbod",
+  dependencies = {
+    "kristijanhusak/vim-dadbod-completion",
+    "kristijanhusak/vim-dadbod-ui",
+  },
+  keys = {
+    { "<leader>bt", "<cmd>DBUIToggle<CR>", desc = "DadBod UI Toggle" },
+  },
+}
