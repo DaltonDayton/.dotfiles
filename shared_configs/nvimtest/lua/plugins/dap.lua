23e00afb1f93d6bdb0a112a15e7ebb3d217747f8
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-10-26 22:53:17 -0600

    change dap keybinds to f789

--- shared_configs/nvimtest/lua/plugins/dap.lua
+++ shared_configs/nvimtest/lua/plugins/dap.lua
@@ -19,9 +19,9 @@ return {
 
     -- Keybinds
     vim.keymap.set("n", "<F5>", function() require("dap").continue() end)
-    vim.keymap.set("n", "<F10>", function() require("dap").step_over() end)
-    vim.keymap.set("n", "<F11>", function() require("dap").step_into() end)
-    vim.keymap.set("n", "<F12>", function() require("dap").step_out() end)
+    vim.keymap.set("n", "<F7>", function() require("dap").step_over() end)
+    vim.keymap.set("n", "<F8>", function() require("dap").step_into() end)
+    vim.keymap.set("n", "<F9>", function() require("dap").step_out() end)
     vim.keymap.set("n", "<Leader>db", function() require("dap").toggle_breakpoint() end)
     vim.keymap.set("n", "<Leader>dp", function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end)
     vim.keymap.set("n", "<Leader>dr", function() require("dap").repl.open() end)
