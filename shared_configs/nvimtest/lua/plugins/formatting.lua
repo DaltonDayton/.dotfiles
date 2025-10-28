f36e5a53f528866c75827a5c7dcd6580f64a2ef9
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-10-27 23:04:03 -0600

    add htmldjango formatting

--- shared_configs/nvimtest/lua/plugins/formatting.lua
+++ shared_configs/nvimtest/lua/plugins/formatting.lua
@@ -13,6 +13,7 @@ return {
         svelte = { "prettier" },
         css = { "prettier" },
         html = { "prettier" },
+        htmldjango = { "prettier" },
         json = { "prettier" },
         yaml = { "prettier" },
         markdown = { "prettier" },
