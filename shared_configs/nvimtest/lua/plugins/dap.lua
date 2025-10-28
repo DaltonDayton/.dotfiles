4860bdbfff9ba2155139bf0f4b9fa124e613301d
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-10-27 23:03:38 -0600

    fix duplicate dap keybind

--- shared_configs/nvimtest/lua/plugins/dap.lua
+++ shared_configs/nvimtest/lua/plugins/dap.lua
@@ -27,7 +27,7 @@ return {
     vim.keymap.set("n", "<Leader>dr", function() require("dap").repl.open() end)
     vim.keymap.set("n", "<Leader>dl", function() require("dap").run_last() end)
     vim.keymap.set({ "n", "v" }, "<Leader>dh", function() require("dap.ui.widgets").hover() end)
-    vim.keymap.set({ "n", "v" }, "<Leader>dp", function() require("dap.ui.widgets").preview() end)
+    vim.keymap.set({ "n", "v" }, "<Leader>dP", function() require("dap.ui.widgets").preview() end)
     vim.keymap.set("n", "<Leader>df", function()
       local widgets = require("dap.ui.widgets")
       widgets.centered_float(widgets.frames)
