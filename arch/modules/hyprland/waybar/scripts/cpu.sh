e0c63c83b2c879ca79f249810e007a3ddc8a165d
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2026-01-17 13:36:42 -0700

    fix formatting

--- arch/modules/hyprland/waybar/scripts/cpu.sh
+++ arch/modules/hyprland/waybar/scripts/cpu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # CPU usage and temperature script for waybar
 
-# Get CPU usage (percentage)
-CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}' | awk '{printf "%3.0f", $1}')
+# Get CPU usage (percentage, integer)
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "%3.0f", 100 - $1}')
 
-# Get CPU temperature
-CPU_TEMP=$(sensors | grep "Tctl:" | awk '{print $2}' | sed 's/+//;s/°C//')
+# Get CPU temperature (integer, rounded)
+CPU_TEMP=$(sensors | grep "Tctl:" | awk '{printf "%2.0f", $2}' | sed 's/\.0//')
 
-# Format output
-printf "<span size=\"13000\" foreground=\"#cba6f7\">⬡</span> %s%% %s°C" "$CPU_USAGE" "$CPU_TEMP"
+# Format output with fixed width using monospace for numbers
+printf "<span size=\"13000\" foreground=\"#cba6f7\">⬡</span> <span font_family=\"monospace\">%s%% %s°C</span>" "$CPU_USAGE" "$CPU_TEMP"
